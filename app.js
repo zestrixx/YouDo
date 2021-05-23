794e18345a3c7b8f9b763898d74ade2006032262
zestrixx <mayank01.kv@gmail.com>
2021-05-23 17:41:21 +0530

    showing user specific tasks only

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 const form = document.querySelector('#add-todo-form')
 const todoList = document.querySelector('#todo-list')
 const updateBtn = document.querySelector('#update')
-let newTitle=''
-let updateId=null
+const logoutItems = document.querySelectorAll('.logged-out')
+const loginItems = document.querySelectorAll('.logged-in')
+let currentUser = null
+let newTitle = ''
+let updateId = null
+
+function setUI(user) {
+    if (user) {
+        loginItems.forEach(item => item.style.display = 'block')
+        logoutItems.forEach(item => item.style.display = 'none')
+    }
+    else {
+        loginItems.forEach(item => item.style.display = 'none')
+        logoutItems.forEach(item => item.style.display = 'block')
+    }
+}
+
 function renderList(doc) {
     let li = document.createElement("li")
     li.className = "collection-item"
@@ -26,41 +41,53 @@ function renderList(doc) {
     li.appendChild(div)
     deleteBtn.addEventListener('click', e => {
         let id = e.target.parentElement.parentElement.getAttribute('data-id')
-        db.collection('todos').doc(id).delete()
+        db.collection('alltodos').doc(currentUser.uid).collection('todos').doc(id).delete()
     })
     editBtn.addEventListener('click', e => {
-        updateId=e.target.parentElement.parentElement.parentElement.getAttribute('data-id')
+        updateId = e.target.parentElement.parentElement.parentElement.getAttribute('data-id')
     })
     todoList.append(li)
 }
-updateBtn.addEventListener('click',e=>{
-    newTitle=document.getElementsByName('newtitle')[0].value
-    db.collection('todos').doc(updateId).update({
+
+updateBtn.addEventListener('click', e => {
+    newTitle = document.getElementsByName('newtitle')[0].value
+    db.collection('alltodos').doc(currentUser.uid).collection('todos').doc(updateId).update({
         title: newTitle
     })
 })
 
 form.addEventListener('submit', e => {
     e.preventDefault()
-    db.collection('todos').add({
+    db.collection('alltodos').doc(currentUser.uid).collection('todos').add({
         title: form.title.value
     })
     form.title.value = ""
 })
 
-db.collection('todos').orderBy('title').onSnapshot(snapshot => {
-    let changes = snapshot.docChanges()
-    changes.forEach(change => {
-        if (change.type == 'added') {
-            renderList(change.doc)
-        }
-        else if (change.type == 'removed') {
-            let li = todoList.querySelector(`[data-id=${change.doc.id}]`)
-            todoList.removeChild(li)
-        }
-        else if (change.type == 'modified') {
-            let li = todoList.querySelector(`[data-id=${change.doc.id}]`)
-            li.getElementsByName('span')[0].textContent = newTitle
-        }
-    });
-})
+
+function getTodos() {
+    todoList.innerHTML = ''
+    currentUser = auth.currentUser
+    document.querySelector('#user-email').innerHTML = (currentUser != null ? currentUser.email : '')
+    if (currentUser === null) {
+        todoList.innerHTML = '<h3 class="center-align">Had you gone for a while! <br>Please login to continue</h3>'
+        return
+    }
+
+    db.collection('alltodos').doc(currentUser.uid).collection('todos').orderBy('title').onSnapshot(snapshot => {
+        let changes = snapshot.docChanges()
+        changes.forEach(change => {
+            if (change.type == 'added') {
+                renderList(change.doc)
+            }
+            else if (change.type == 'removed') {
+                let li = todoList.querySelector(`[data-id=${change.doc.id}]`)
+                todoList.removeChild(li)
+            }
+            else if (change.type == 'modified') {
+                let li = todoList.querySelector(`[data-id=${change.doc.id}]`)
+                li.getElementsByName('span')[0].textContent = newTitle
+            }
+        });
+    })
+}
--- auth.js
+++ auth.js
@@ -1,15 +1,45 @@
-const signupForm = document.querySelector('#signup-form')
-signupForm.addEventListener('submit', e=>{
+auth.onAuthStateChanged(user => {
+    setUI(user)
+    getTodos()
+})
+
+//############### Login ##############
+const loginForm = document.querySelector('#login-form')
+loginForm.addEventListener('submit', e => {
     e.preventDefault()
-    const email=signupForm['signup-email'].value
+    const email = loginForm['login-email'].value
+    const password = loginForm['login-password'].value
+    auth.signInWithEmailAndPassword(email, password).then(() => {
+        const modal = document.querySelector('#modal-login')
+        loginForm.querySelector('.error').innerHTML = ''
+        M.Modal.getInstance(modal).close()
+        loginForm.reset()
+    })
+        .catch(err => {
+            loginForm.querySelector('.error').innerHTML = err.message
+        })
+})
+
+//############## logout ##############
+const logout = document.querySelector('#logout')
+logout.addEventListener('click', e => {
+    e.preventDefault()
+    auth.signOut()
+})
+
+//############### Signup ##############
+const signupForm = document.querySelector('#signup-form')
+signupForm.addEventListener('submit', e => {
+    e.preventDefault()
+    const email = signupForm['signup-email'].value
     const password = signupForm['signup-password'].value
-    auth.createUserWithEmailAndPassword(email, password).then(()=>{
-        const modal =document.querySelector('#modal-signup')
-        signupForm.querySelector('.error').innerHTML=''
+    auth.createUserWithEmailAndPassword(email, password).then(() => {
+        const modal = document.querySelector('#modal-signup')
+        signupForm.querySelector('.error').innerHTML = ''
         M.Modal.getInstance(modal).close()
         signupForm.reset()
     })
-    .catch(err=>{
-        signupForm.querySelector('.error').innerHTML=err.message
-    })
+        .catch(err => {
+            signupForm.querySelector('.error').innerHTML = err.message
+        })
 })
